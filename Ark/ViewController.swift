892d36f82c0f2b84650716af8ef1a8f4233dd70b
Sangchan <sangchan82@gmail.com>
2021-03-21 14:44:48 +0900

    simple rx

--- Ark/ViewController.swift
+++ Ark/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
-
+    private var vm : BehaviorSubject<ViewModel>? = .init(value: .init(title: "created"))
+    lazy var disposeBag : DisposeBag = DisposeBag()
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        vm?.asObservable().subscribe { [unowned self] (vm) in
+            self.label.text = vm.title
+        }.disposed(by: disposeBag)
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        vm?.onNext(.init(title: "Hello World Again!"))
+    }
+    
+    deinit {
+        print("deinit ViewController")
+    }
 }
 
